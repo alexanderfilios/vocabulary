4504c584fd0c24236b4e117234404014ea8c8d86
Richard Park <objectiv@gmail.com>
2016-04-10 01:38:42 -0400

    chore(build): update webpack config

--- webpack.dist.js
+++ webpack.dist.js
@@ -1,75 +1,32 @@
-const path = require('path');
 const webpack = require('webpack');
-const config = require('./webpack.base');
+const webpackBase = require('./webpack.base');
+const config = webpackBase.config;
+const loaders = webpackBase.loaders;
 
 // plugins
-const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DedupePlugin = webpack.optimize.DedupePlugin;
-const DefinePlugin = webpack.DefinePlugin;
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OccurenceOrderPlugin = webpack.optimize.OccurenceOrderPlugin;
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 
-module.exports = {
-  cache: true,
-  debug: true,
-  devtool: 'source-map',
-  entry: config.entry,
-  output: config.output,
-  resolve: config.resolve,
-  postcss: config.postcss,
-  sassLoader: config.sassLoader,
+module.exports = config;
 
-  module: {
-    loaders: [
-      {test: /\.html$/, loader: 'raw'},
-      {test: /\.ts$/, exclude: [/\.spec\.ts$/, /node_modules/], loader: 'ts'},
-      {test: /\.scss$/, include: [path.resolve(__dirname, 'src/components')], loader: 'raw!postcss-loader!sass'},
-      {test: /\.scss$/, include: [path.resolve(__dirname, 'src/styles')], loader: ExtractTextPlugin.extract('css!postcss-loader!sass')}
-    ],
+config.module.loaders.push(
+  loaders.scss.extract
+);
 
-    noParse: config.module.noParse
-  },
-
-  plugins: [
-    new DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new ExtractTextPlugin('styles.css'),
-    new DedupePlugin(),
-    new OccurenceOrderPlugin(),
-    new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
-    new CopyWebpackPlugin(config.assets),
-    new HtmlWebpackPlugin({
-      chunksSortMode: 'none',
-      filename: 'index.html',
-      hash: true,
-      inject: 'body',
-      template: './src/index.html'
-    }),
-    new UglifyJsPlugin({
-      compress: {
-        dead_code: true, // eslint-disable-line camelcase
-        screw_ie8: true, // eslint-disable-line camelcase
-        unused: true,
-        warnings: false
-      },
-      mangle: false
-    })
-  ],
-
-  stats: {
-    cached: true,
-    cachedAssets: true,
-    chunks: true,
-    chunkModules: true,
-    colors: true,
-    hash: false,
-    reasons: false,
-    timings: true,
-    version: false
-  }
-};
+config.plugins.push(
+  new ExtractTextPlugin('styles.css'),
+  new DedupePlugin(),
+  new OccurenceOrderPlugin(),
+  new UglifyJsPlugin({
+    compress: {
+      dead_code: true, // eslint-disable-line camelcase
+      screw_ie8: true, // eslint-disable-line camelcase
+      unused: true,
+      warnings: false
+    },
+    mangle: false
+  })
+);
